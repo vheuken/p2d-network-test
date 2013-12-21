f35e0bc99bae4e5d071032107ea6e05a063e0280
Vincent Heuken <vincent@vincentheuken.com>
2013-12-21 14:39:58 -0800

    added stuff

--- scenes/init/onPhysicsUpdate.lua
+++ scenes/init/onPhysicsUpdate.lua
@@ -2,6 +2,9 @@ local startup = true
 local sending = true
 local receiving = false
 
+local recipient = "192.168.1.218"
+local sender = "192.168.1.88"
+
 function onPhysicsUpdate()
     if startup == true then
         print("Local IP Address: " .. GPurityNetwork:getLocalAddress())
@@ -10,11 +13,11 @@ function onPhysicsUpdate()
     end
     
     if sending == true then
-       
+       GPurityNetwork:send(recipient)
     end
     
     if receiving == true then
-    
+        GPurityNetwork:receive()
     end
 
 end
