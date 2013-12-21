74f20c561a95facf43ddb3a36e9c493ebd9a0d9c
Vincent Heuken <vincent@vincentheuken.com>
2013-12-21 14:24:50 -0800

    added some stuff

--- scenes/init/onPhysicsUpdate.lua
+++ scenes/init/onPhysicsUpdate.lua
@@ -1,10 +1,20 @@
 local startup = true
+local sending = true
+local receiving = false
 
 function onPhysicsUpdate()
     if startup == true then
-        print(GPurityNetwork:getLocalAddress())
-        print(GPurityNetwork:getPublicAddress())
+        print("Local IP Address: " .. GPurityNetwork:getLocalAddress())
+        print("Public IP Address: " .. GPurityNetwork:getPublicAddress())
         startup = false
+    end
+    
+    if sending == true then
+       
+    end
+    
+    if receiving == true then
+    
     end
 
 end
